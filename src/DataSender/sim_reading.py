615d043c31ed7b701c825d680c0d75ae5773e4e4
ryuever <liuyouchao111@gmail.com>
2015-02-02 12:23:31 +0900

    current time as initial screen

--- src/DataSender/sim_reading.py
+++ src/DataSender/sim_reading.py
@@ -1,4 +1,6 @@
 # read from xml file, every 0.1s one record
+# NumberSamples : how many point to display in the figure
+
 
 import os, sys
 import threading
@@ -13,10 +15,6 @@ import Tkinter
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
 
-time_data = []
-
-value = []
-first_line = False
 class Mythread(threading.Thread):
     def __init__(self, myId):
         self.myId = myId
@@ -26,19 +24,16 @@ class Mythread(threading.Thread):
         global  time_data, value, first_line
         exec_path = os.path.dirname(os.path.abspath(__file__))
         src_file_path = os.path.join(exec_path, '../../' , "SampleDATA/xml_files/")
-        # print "src_file_path : ", src_file_path
 
         with open(src_file_path + 'First.xml') as fp:
             for line in fp:
                 time.sleep(0.2)
-                # print line
                 t_data ='<root>' + line.decode() + '</root>'
                 root = ET.fromstring(t_data)
                 
                 date = ''
                 for item in root:
                     date = item.attrib["date"] + ' ' + item.attrib['time']
-                    # print date
                     py_date = datetime.datetime.strptime(date, '%Y%m%d %H:%M')
                     print py_date
                     if first_line == False:
@@ -54,25 +49,39 @@ class Mythread(threading.Thread):
 threads = []
 thread = Mythread(1)
 thread.start()
+time_data = []
+value = []
+first_line = False
 
+first_time = False
 root = Tkinter.Tk()
 root.wm_title("Extended Realtime Plotter")
-
-xAchse=pylab.arange(0,100,1)
-yAchse=pylab.array([0]*100)
 
 fig = pylab.figure(1)
 ax = fig.add_subplot(111)
 ax.grid(True)
 ax.set_title("Realtime Simulation")
-
 ax.set_ylabel("generated power")
-# ax.axis([0,100,-1.5,1.5])
-# ax.plot(xAchse,yAchse,'-')
+first_time = True
+line1 = ''
+# line1 = ax.plot(time_data, value, '-')
+# line1 = ax.plot(datetime.datetime.now(), np.nan, '-')
+# line1 = ax.plot(time_data, value, '-')
 
-# plt.plot([1,2,3], [3,4,5])
-line1 = ax.plot(time_data, value, '-')
-# line1=ax.plot(xAchse,yAchse,'-')
+init_date = []
+init_value = []
+
+for i in reversed(range(100)):
+    init_date.append(datetime.datetime.now() - timedelta(minutes=i))
+    init_value.append(np.nan)
+# print init_date, init_value
+# ax.axis([init_date[0], init_date[-1], 0, 100])    
+# ax.xaxis.set_major_formatter(DateFormatter('%H:%M') )
+# ax.xaxis.set_major_locator(MinuteLocator(interval=5))
+# plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+# plt.subplots_adjust(bottom=0.2)
+# ax.set_xlabel(datetime.datetime.now().strftime("%Y-%m-%d"))
+line1 = ax.plot(init_date, init_value, '-')
 
 canvas = FigureCanvasTkAgg(fig, master=root)
 canvas.show()
@@ -83,14 +92,14 @@ toolbar.update()
 canvas._tkcanvas.pack(side=Tkinter.TOP, fill=Tkinter.BOTH, expand=1)
 
 def RealtimePloter():
-    global value,wScale,wScale2
+    print "begin"
+    global value,wScale,wScale2, first_time, line1
     NumberSamples=min(len(value),wScale.get())
     
-    print value
-    print time_data
     len_value = len(value)
     m_left_index = len_value-NumberSamples
     m_right_index = len_value - 1
+    # line1[0].set_data(time_data[-NumberSamples:], value[-NumberSamples:])
     line1[0].set_data(time_data[-NumberSamples:], value[-NumberSamples:])
     ax.axis([time_data[m_left_index], time_data[m_right_index], 0, 100])    
 
@@ -115,6 +124,7 @@ def RealtimePloter():
             date_str = (m_left_date + timedelta(days=i)).strftime("%Y-%m-%d")            
             list_x_label.append(date_str)
         ax.set_xlabel("   ".join(list_x_label))
+    print('canvas draw()')
     canvas.draw()
     root.after(1000,RealtimePloter)
 

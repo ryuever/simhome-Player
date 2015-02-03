ca15749f4614be0bb71ddf5c63a01ab6acfcd585
ryuever <liuyouchao111@gmail.com>
2015-02-03 12:08:07 +0900

    add time frequency radiobutton

--- src/DataSender/sim_reading.py
+++ src/DataSender/sim_reading.py
@@ -1,7 +1,6 @@
 # read from xml file, every 0.1s one record
 # NumberSamples : how many point to display in the figure
 
-
 import os, sys
 import threading
 import xml.etree.ElementTree as ET
@@ -12,6 +11,7 @@ import matplotlib.dates as md
 import pylab
 from pylab import *
 import Tkinter
+from Tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
 
@@ -25,7 +25,7 @@ class Mythread(threading.Thread):
         exec_path = os.path.dirname(os.path.abspath(__file__))
         src_file_path = os.path.join(exec_path, '../../' , "SampleDATA/xml_files/")
 
-        with open(src_file_path + 'First.xml') as fp:
+        with open(src_file_path + 'First2.xml') as fp:
             for line in fp:
                 time.sleep(0.2)
                 t_data ='<root>' + line.decode() + '</root>'
@@ -74,13 +74,13 @@ init_value = []
 for i in reversed(range(100)):
     init_date.append(datetime.datetime.now() - timedelta(minutes=i))
     init_value.append(np.nan)
-# print init_date, init_value
-# ax.axis([init_date[0], init_date[-1], 0, 100])    
-# ax.xaxis.set_major_formatter(DateFormatter('%H:%M') )
-# ax.xaxis.set_major_locator(MinuteLocator(interval=5))
-# plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-# plt.subplots_adjust(bottom=0.2)
-# ax.set_xlabel(datetime.datetime.now().strftime("%Y-%m-%d"))
+print init_date, init_value
+ax.axis([init_date[0], init_date[-1], 0, 100])    
+ax.xaxis.set_major_formatter(DateFormatter('%H:%M') )
+ax.xaxis.set_major_locator(MinuteLocator(interval=5))
+plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.subplots_adjust(bottom=0.2)
+ax.set_xlabel(datetime.datetime.now().strftime("%Y-%m-%d"))
 line1 = ax.plot(init_date, init_value, '-')
 
 canvas = FigureCanvasTkAgg(fig, master=root)
@@ -93,8 +93,9 @@ canvas._tkcanvas.pack(side=Tkinter.TOP, fill=Tkinter.BOTH, expand=1)
 
 def RealtimePloter():
     print "begin"
-    global value,wScale,wScale2, first_time, line1
-    NumberSamples=min(len(value),wScale.get())
+    global value,wRadio, first_time, line1, v
+    radio_value = v.get()
+    NumberSamples=min(len(value),radio_value)
     
     len_value = len(value)
     m_left_index = len_value-NumberSamples
@@ -106,7 +107,9 @@ def RealtimePloter():
     # ax.xaxis.set_major_formatter( DateFormatter('%Y-%m-%d %H:%M') )
     # time only
     ax.xaxis.set_major_formatter(DateFormatter('%H:%M') )
-    ax.xaxis.set_major_locator(MinuteLocator(interval=5))
+    
+    numDict2 = {'20':1, '100':5, '200':10, '300':15, '600':30}
+    ax.xaxis.set_major_locator(MinuteLocator(interval=numDict2[str(radio_value)]))
     plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
     plt.subplots_adjust(bottom=0.2)
 
@@ -136,37 +139,16 @@ def _quit():
 button = Tkinter.Button(master=root, text='Quit', command=_quit)
 button.pack(side=Tkinter.BOTTOM)
 
-wScale = Tkinter.Scale(master=root,label="View Width:", from_=3,
-                       to=1000,sliderlength=30, orient=Tkinter.HORIZONTAL)
-wScale2 = Tkinter.Scale(master=root,label="Generation Speed:", from_=1,
-                        to=200,sliderlength=30, orient=Tkinter.HORIZONTAL)
-wScale2.pack(side=Tkinter.BOTTOM)
-wScale.pack(side=Tkinter.BOTTOM)
+Label(master=root, text="Choose a time frequency").pack(side=TOP)
+numList = [('1', 20), ('5', 100), ('10', 200), ('15', 300), ('30', 600)]
+v = IntVar()
+v.set(100)
+for radio_text, radio_value in numList:
+    print "loop"
+    wRadio=Tkinter.Radiobutton(master=root,text=radio_text,value=radio_value,variable =v)
+    wRadio.pack(side=LEFT, anchor=W)
 
-wScale.set(100)
-wScale2.set(wScale2['to']-10)
-
-root.protocol("WM_DELETE_WINDOW", _quit)  #thanks aurelienvlg
-# root.after(100,SinwaveformGenerator)
+root.protocol("WM_DELETE_WINDOW", _quit)
 root.after(1000,RealtimePloter)
 Tkinter.mainloop()
-#pylab.show()
-
-
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111)
-# y_min, y_max = ax.get_ylim()
-# ticks = [(tick - y_min)/(y_max - y_min) for tick in ax.get_yticks()]
-# ax.tick_params(direction='out', length=6, width=2, colors='r')
-# x = [1, 3, 5]
-# y = [4, 5, 7]
-# plt.plot(x,y)
-
-
-# ax.xaxis.set_tick_params(width=15)
-# ax.tick_params(direction='out', length=6, width=2, colors='r')
-
-# ax.tick_params(direction='out', pad=50)
-# >>> plt.show()
 
